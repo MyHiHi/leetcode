c99cc1e381f788e1886a68a1a02f151431619f00
MyHiHi <909535692@qq.com>
2020-02-05 10:43:26 +0800

    迷路的牛牛

--- test.py
+++ test.py
@@ -1,15 +1,2 @@
-import sys
-lines = sys.stdin.readlines()
-lines = [line.strip() for line in lines if line.strip()]
-l, r = map(int, lines[0].split())
-
-
-def get(m):
-    su = m//3*2
-    if m % 3 == 2:
-      su += 1
-      return su 
-    return su
-
-
-print(get(r)-get(l-1))
+for i in range(-5, 5):
+    print(i % 5)
