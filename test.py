980cefc42b9bde670f75cf35726a1a074eb02d88
MyHiHi <909535692@qq.com>
2020-02-06 17:05:18 +0800

    数对

--- test.py
+++ test.py
@@ -1,10 +1,15 @@
-p = ["a1 9 2 3 1", "g1 act car", "zo4 4 7", "ab1 off key dog", "a8 act zoo"]
 
-
-def r(log):
-    _id, rest = log.split(" ", 1)
-    return (0, rest, _id) if rest[0].isalpha() else (1,)
-
-
-ry = sorted(p, key=r)
-print(ry)
+import sys
+c = sys.stdin.readlines()
+n, k = map(int, c[0].split())
+co = 0
+if k == 0:
+    print(n*n)
+else:
+    for i in range(k+1, n+1):
+        a1 = i-k
+        a2 = n//i
+        a3 = n % i-k+1
+        print('--->: ', a1, a2, a3)
+        co += a1*a2+max(a3, 0)
+    print(co)
