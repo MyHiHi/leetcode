28ed4f487f87857cda3c53630fa173f3575c2a7d
MyHiHi <909535692@qq.com>
2020-02-07 10:54:22 +0800

    矩阵重叠

--- test.py
+++ test.py
@@ -1,15 +1,17 @@
-
 import sys
 c = sys.stdin.readlines()
-n, k = map(int, c[0].split())
-co = 0
-if k == 0:
-    print(n*n)
-else:
-    for i in range(k+1, n+1):
-        a1 = i-k
-        a2 = n//i
-        a3 = n % i-k+1
-        print('--->: ', a1, a2, a3)
-        co += a1*a2+max(a3, 0)
-    print(co)
+n = int(c[0])
+x1 = list(map(int, c[1].split()))
+y1 = list(map(int, c[2].split()))
+x2 = list(map(int, c[3].split()))
+y2 = list(map(int, c[4].split()))
+res = 0
+for x in x1:
+    for y in y1:
+        co = 0
+        for i in range(n):
+            if x >= x1[i] and x < x2[i] and y >= y1[i] and y < x2[i]:
+                co += 1
+        res = max(co, res)
+
+print(res)
