c3f6865970765d1031b4ceefb686ad882aeeebf1
MyHiHi <909535692@qq.com>
2020-02-16 12:55:34 +0800

    小易的字典

--- test.py
+++ test.py
@@ -1,17 +1,33 @@
-
-def out(arr,start,end,res):
-  if start==end:
-    res+=[arr[:]];
-  else:
-    for i in range(start,end):
-      arr[i],arr[start]=arr[start],arr[i];
-      out(arr,start+1,end,res);
-      arr[i],arr[start]=arr[start],arr[i];
+# n, m, k = list(map(int, input().strip().split()))
+# arr = ['a']*n+['z']*m
 
 
-lis = [1, 2, 3]
-res = []
-out(lis, 0,len(lis), res)
-print(res)
+# def rev(arr, s, e, res):
+#     if s == e:
+#         res.add(''.join(map(str,arr[:])))
+#     else:
+#         for i in range(s, e):
+#             arr[i], arr[s] = arr[s], arr[i]
+#             rev(arr, s+1, e, res)
+#             arr[i], arr[s] = arr[s], arr[i]
 
 
+# res = set()
+# rev(arr, 0, len(arr), res)
+# print(res)
+# list(res).sort()
+# # res.sort()2 2 6
+# print(res, len(res))
+# # print(''.join(map(str, res[k])))
+
+
+def bitwiseComplement(N: int) -> int:
+    c = bin(N)[2:]
+    p = ""
+    for i in c:
+        p += '1' if i == '0' else '0'
+    print(c, p)
+    return int(p, 2)
+
+
+print(bitwiseComplement(10))
