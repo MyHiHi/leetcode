fd8b34ec8906be603dc99b520b9cb28be543e15e
MyHiHi <909535692@qq.com>
2020-02-17 09:45:35 +0800

    见名知义

---
+++ 1047. 删除字符串中的所有相邻重复项.py
@@ -0,0 +1,9 @@
+# -*- encoding: utf-8 -*-
+'''
+@File    :   1047. 删除字符串中的所有相邻重复项.py
+@Time    :   2020/02/17 09:29:37
+@Author  :   Zhang tao 
+@Version :   1.0
+@Desc    :   1047. 删除字符串中的所有相邻重复项.py
+'''
+
--- test.py
+++ test.py
@@ -1,33 +1,19 @@
-# n, m, k = list(map(int, input().strip().split()))
-# arr = ['a']*n+['z']*m
+def removeDuplicates(S: str) -> str:
+    i, le = 1, len(S)
+    preIndex = 0
+    while i < le:
+        val = S[i]
+        if val != S[preIndex]:
+            preIndex = i
+            i += 1
+        else:
+            S = S[0:preIndex]+(S[i+1:] if i+1 < le else "")
+            print('>>', preIndex, i+1)
+            print("S:", S)
+            le = len(S)
+            i = 1
+            preIndex = 0
+    return S
 
 
-# def rev(arr, s, e, res):
-#     if s == e:
-#         res.add(''.join(map(str,arr[:])))
-#     else:
-#         for i in range(s, e):
-#             arr[i], arr[s] = arr[s], arr[i]
-#             rev(arr, s+1, e, res)
-#             arr[i], arr[s] = arr[s], arr[i]
-
-
-# res = set()
-# rev(arr, 0, len(arr), res)
-# print(res)
-# list(res).sort()
-# # res.sort()2 2 6
-# print(res, len(res))
-# # print(''.join(map(str, res[k])))
-
-
-def bitwiseComplement(N: int) -> int:
-    c = bin(N)[2:]
-    p = ""
-    for i in c:
-        p += '1' if i == '0' else '0'
-    print(c, p)
-    return int(p, 2)
-
-
-print(bitwiseComplement(10))
+print(removeDuplicates("aababaab"))
