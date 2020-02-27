6e56c3b7d9b89d733169d59bdcd04b1e958f35c8
MyHiHi <909535692@qq.com>
2020-02-27 10:52:17 +0800

    游戏海报

--- test.py
+++ test.py
@@ -1,15 +1,3 @@
-import math
-N,P,H,W=list(map(int,input().strip().split()));
-pWord=list(map(int,input().strip().split()));
-S=1;
-while True:
-    w,h=W//S,H//S;
-    if w==0 or h==0:break;
-    cols=0;
-    for i in range(N):
-        cols+=math.ceil(pWord[i]/w);
-    if cols>h*P:break;
-    S+=1;
-print(S-1)
-        
-    
+import sys
+p = sys.stdin.readlines()
+print('>>>：', p)
